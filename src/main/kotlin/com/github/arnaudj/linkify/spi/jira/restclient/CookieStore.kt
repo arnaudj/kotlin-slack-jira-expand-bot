d86d1afe310790b04e323070998fb13050b9a206
Arnaud J <arnaudj@users.noreply.github.com>
2019-06-19 16:38:32 +0100

    Misc changes on loadForRequest (completes PR #2)
    Expired cookies are removed from the cookie jar
    loadForRequest is now used to propagate session cookie transparently

--- src/main/kotlin/com/github/arnaudj/linkify/spi/jira/restclient/CookieStore.kt
+++ src/main/kotlin/com/github/arnaudj/linkify/spi/jira/restclient/CookieStore.kt
@@ -3,27 +3,37 @@ package com.github.arnaudj.linkify.spi.jira.restclient
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
-import java.time.Duration
 
 open class CookieStore : CookieJar {
 
-    private final val cookieStore : Set<Cookie> = hashSetOf<Cookie>();
+    private val cookieStore: MutableSet<Cookie> = mutableSetOf()
 
-    override fun loadForRequest(url: HttpUrl): MutableList<Cookie> {
-        val  validCookies : ArrayList<Cookie> = arrayListOf<Cookie>();
-        cookieStore.forEach {
-            if(it.expiresAt() >  Duration.ofHours(8).toMillis()) {
-                validCookies.add(it)
-            }
+    @Synchronized
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        clearCache()
+        return cookieStore.toList()
+    }
+
+    @Synchronized
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        cookies.forEach {
+            cookieStore.add(it)
         }
-        return validCookies;
     }
 
-    override open fun saveFromResponse(url : HttpUrl, cookies : List<Cookie>) {
-        cookieStore.plus(cookies)
+    @Synchronized
+    open fun hasValidSessionCookie(): Boolean {
+        clearCache()
+        return cookieStore.find { it.name() == "JSESSIONID" } != null
     }
 
-    open fun getCookieStore(): Set<Cookie>{
-        return cookieStore;
+    @Synchronized
+    private fun clearCache() {
+        val now = System.currentTimeMillis()
+        cookieStore.removeIf { it.expiresAt() <= now }
+    }
+
+    override fun toString(): String {
+        return cookieStore.toString()
     }
 }
