6abb4d99077772468ad8c67693f9d0f83d0b6ed9
Arnaud J <arnaudj@users.noreply.github.com>
2019-06-20 16:54:49 +0100

    Ensure JSESSIONID cookie expiration is set even when not issued by server

--- src/main/kotlin/com/github/arnaudj/linkify/spi/jira/restclient/CookieStore.kt
+++ src/main/kotlin/com/github/arnaudj/linkify/spi/jira/restclient/CookieStore.kt
@@ -3,34 +3,56 @@ package com.github.arnaudj.linkify.spi.jira.restclient
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
+import java.util.concurrent.TimeUnit
+
+/**
+ * Custom [Cookie] wrapper with overridable expiresAt
+ */
+class CookieEx(val cookie: Cookie, private val expiresAt: Long?) {
+    fun isExpired(): Boolean {
+        val now = System.currentTimeMillis()
+        return if (expiresAt != null)
+            expiresAt <= now
+        else
+            cookie.expiresAt() <= now
+    }
+
+    override fun toString(): String {
+        return "[expiresAt=$expiresAt, cookie=$cookie]"
+    }
+}
 
 open class CookieStore : CookieJar {
-
-    private val cookieStore: MutableSet<Cookie> = mutableSetOf()
+    private val sessionCookieName = "JSESSIONID"
+    private val sessionCookieExpiryHours = 8L
+    private val cookieStore: MutableSet<CookieEx> = mutableSetOf()
 
     @Synchronized
     override fun loadForRequest(url: HttpUrl): List<Cookie> {
         clearCache()
-        return cookieStore.toList()
+        return cookieStore.toList().map { it.cookie }
     }
 
     @Synchronized
     override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
-        cookies.forEach {
-            cookieStore.add(it)
+        cookies.forEach { cookie ->
+            var expires = cookie.expiresAt()
+            if (cookie.name() == sessionCookieName) { // no expiresAt provided, add it
+                expires = System.currentTimeMillis() + TimeUnit.HOURS.toMillis(sessionCookieExpiryHours)
+            }
+            cookieStore.add(CookieEx(cookie, expires))
         }
     }
 
     @Synchronized
     open fun hasValidSessionCookie(): Boolean {
         clearCache()
-        return cookieStore.find { it.name() == "JSESSIONID" } != null
+        return cookieStore.find { it.cookie.name() == sessionCookieName } != null
     }
 
     @Synchronized
     private fun clearCache() {
-        val now = System.currentTimeMillis()
-        cookieStore.removeIf { it.expiresAt() <= now }
+        cookieStore.removeIf { it.isExpired() }
     }
 
     override fun toString(): String {
